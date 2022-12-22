8f9506ef7f4f4e8793cce9419436bd04d483391b
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-12-22 08:19:08 +0530

    doc

--- app/src/main/java/com/example/getfitnav/AddIntake.java
+++ app/src/main/java/com/example/getfitnav/AddIntake.java
@@ -1,6 +1,7 @@
 package com.example.getfitnav;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
